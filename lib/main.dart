e2d3777e69ba416f9458b90b88f9abb875d6ff15
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-17 08:41:53 +0100

    adde row with Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,18 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: const [
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            )
+          ],
+        )
       ],
     );
   }
