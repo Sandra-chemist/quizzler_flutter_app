8fe36bf741d6ed7fd6986b32f92cf3792e97028c
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-17 09:25:34 +0100

    added List with questions and cleaned List with scoreKeeper

--- lib/main.dart
+++ lib/main.dart
@@ -25,19 +25,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
 
   @override
@@ -77,14 +69,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(
-                    const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                }); //The user picked true.
+                //The user picked true.
               },
             ),
           ),
