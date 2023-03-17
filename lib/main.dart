adb24e9c02dd9d2a20d85da78db7c824166d9ea5
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-17 10:41:15 +0100

    created a list with answers and a function to check them

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.'
   ];
 
+  List<bool> answers = [false, true, true];
+
   int questionNumber = 0;
 
   @override
@@ -72,6 +74,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
                 setState(() {
                   questionNumber++;
                 });
@@ -95,6 +103,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
                 //The user picked false.
                 setState(() {
                   questionNumber++;
